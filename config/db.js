716fa7c352ed7ed5cec372187e3a61d74736d376
apt-prog <fenilvegad300@gmail.com>
2025-07-15 18:16:33 +0530

    🚀 Perfet Code Working Mode For Live .

--- config/db.js
+++ config/db.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const coonectWithDB = () => {
+export const coonectWithDB = async () => {
   try {
-    mongoose.connect(process.env.MongoDB_Key);
+    await mongoose.connect(process.env.MongoDB_Key, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`Data Base Connected SuccessFully ✅`);
   } catch (err) {
     console.log(`In The DB Connection Have Some Problem ❌ ${err}`);
