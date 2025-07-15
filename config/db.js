007525d4b0925800b1dc2a9383838241fe223bca
FenilVegad <fenilvegad300@gmail.com>
2025-07-15 19:04:41 +0530

    db.js

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ dotenv.config();
 
 export const coonectWithDB = async () => {
   try {
-    await mongoose.connect(process.env.MongoDB_Key, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MongoDB_Key);
     console.log(`Data Base Connected SuccessFully ✅`);
   } catch (err) {
     console.log(`In The DB Connection Have Some Problem ❌ ${err}`);
